60cf4be1f5ce810f7b9ba1baab109f60047b8d5b
Eddie Santiago <santiago.mobarrero@gmail.com>
2025-12-17 19:03:58 -0500

    Create connection to API with the repository and BD

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,54 @@
+import type { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { pool } from '../config/database';
+
+
+export const register = async (req: Request, res: Response) => {
+const { nombre, email, contraseña } = req.body;
+
+
+const hash = await bcrypt.hash(contraseña, 10);
+
+
+await pool.query(
+'INSERT INTO usuarios (nombre, email, contraseña_hash) VALUES ($1, $2, $3)',
+[nombre, email, hash]
+);
+
+
+res.status(201).json({ message: 'Usuario registrado correctamente' });
+};
+
+
+export const login = async (req: Request, res: Response) => {
+const { email, contraseña } = req.body;
+
+
+const result = await pool.query(
+'SELECT * FROM usuarios WHERE email = $1',
+[email]
+);
+
+
+if (result.rowCount === 0) {
+return res.status(401).json({ message: 'Credenciales inválidas' });
+}
+
+
+const usuario = result.rows[0];
+const valido = await bcrypt.compare(contraseña, usuario.contraseña_hash);
+
+
+if (!valido) {
+return res.status(401).json({ message: 'Credenciales inválidas' });
+}
+
+
+const token = jwt.sign({ id: usuario.id_usuario }, process.env.JWT_SECRET!, {
+expiresIn: '1h',
+});
+
+
+res.json({ token });
+};
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import app from './app';
+
+
+app.listen(3000, () => {
+console.log('Servidor corriendo en http://localhost:3000');
+});
